dd21818bcfadeeb975b6cecbfaff343a35e67b73
cachola25 <cachola25@up.edu>
2024-09-14 03:59:43 -0700

    Pause Menu Integration
    Added the pause menu to the submarine
    UI node. Fixed the bug where the pause
    menu would show up when pressing space bar
    after pausing for the first time.

--- pause_menu.gd
+++ pause_menu.gd
@@ -2,23 +2,20 @@ extends Control
 
 func resume():
 	get_tree().paused = false
-	$AnimationPlayer.play_backwards("blur")
+	$AnimationPlayer.play("RESET")
 	
 func paused():
 	get_tree().paused  = true
 	$AnimationPlayer.play("blur")
 	
 func testEsc():
-	if Input.is_action_just_pressed("escape") and get_tree().paused == false:
+	if Input.is_action_just_pressed("escape") and not get_tree().paused:
 		paused()
-	elif Input.is_action_just_pressed("escape") and get_tree().paused == true:
-		resume()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimationPlayer.play("RESET")
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	testEsc()
@@ -27,10 +24,8 @@ func _process(delta: float) -> void:
 func _on_resume_pressed() -> void:
 	resume()
 
-
 func _on_help_pressed() -> void:
 	resume()
-
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
