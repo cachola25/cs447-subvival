90ef537d421655986a8bb9670e4035eea6301833
cachola25 <cachola25@up.edu>
2024-09-28 23:24:11 -0700

    Help Menu updates
    Created a help menu telling the player basic objectives and controls for the game.

--- submarine/UI/pause_menu/pause_menu.gd
+++ submarine/UI/pause_menu/pause_menu.gd
@@ -2,6 +2,8 @@ extends Control
 
 func resume():
 	get_tree().paused = false
+	var submarine_scene = get_tree().root.get_child(0).get_node("submarine")
+	submarine_scene.get_node("CanvasLayer").visible = true
 	$AnimationPlayer.play_backwards("RESET")
 	
 func paused():
@@ -11,6 +13,8 @@ func paused():
 func testEsc():
 	if Input.is_action_just_pressed("escape"):
 		if not get_tree().paused:
+			var submarine_scene = get_tree().root.get_child(0).get_node("submarine")
+			submarine_scene.get_node("CanvasLayer").visible = false
 			paused()
 
 # Called when the node enters the scene tree for the first time.
@@ -27,7 +31,6 @@ func _on_resume_pressed() -> void:
 
 func _on_help_pressed() -> void:
 	get_tree().paused = true
-	
 	var help_menu = load("res://menu_scenes/help_menu/help_scene.tscn").instantiate()
 	help_menu.process_mode = Node.PROCESS_MODE_WHEN_PAUSED
 	add_child(help_menu)
